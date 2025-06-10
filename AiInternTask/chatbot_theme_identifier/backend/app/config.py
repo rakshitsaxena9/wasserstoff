3a1857977a331e4dbe20f2294de22ebb0e06d99b
rakshitsaxena9 <rakshitsaxena9@gmail.com>
2025-06-10 15:43:27 +0530

    Smaller embedding model "paraphrase-MiniLM-L3-v2"

--- AiInternTask/chatbot_theme_identifier/backend/app/config.py
+++ AiInternTask/chatbot_theme_identifier/backend/app/config.py
@@ -6,7 +6,7 @@ load_dotenv(dotenv_path=os.path.join(os.path.dirname(os.path.dirname(__file__)),
 
 # Now use environment variables as before
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-EMBEDDING_MODEL = "all-MiniLM-L6-v2"
+EMBEDDING_MODEL = "paraphrase-MiniLM-L3-v2"
 CHROMA_COLLECTION = "documents"
 UPLOAD_DIR = os.path.join(os.path.dirname(__file__), '..', 'data')
 if not GEMINI_API_KEY:
